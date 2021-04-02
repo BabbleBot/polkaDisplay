8fdefe10dd79efb2529b595b5b01a3ad39d36c1e
BabbleBot <35687156+BabbleBot@users.noreply.github.com>
2021-04-02 23:52:11 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+let polkamonList=document.getElementsByClassName('personalCollectionCardstyle__CardContainer-sc-61434w-0 gxEUbp');
+let reg=/(Chance ~)(.*)(%)/;
+
+for(let polkamon of polkamonList){
+  let proba=1;
+  let spanList=polkamon.getElementsByTagName('span');
+
+  for(let span of spanList){
+    let match = span.textContent.match(reg);
+    if(match){
+      proba*=(match[2]/100);
+    }
+  }
+
+  polkamon.getElementsByClassName('personalCollectionCardstyle__DetailsText-sc-61434w-5 jYnmvc')[0].textContent=(Math.round(1/proba));
+}
