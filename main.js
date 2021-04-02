869a3d2fff43c4ca6a3a556492f9ac7716874955
BabbleBot <35687156+BabbleBot@users.noreply.github.com>
2021-04-03 01:19:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-(() => {
-  if (typeof polkamonList == 'undefined') var polkamonList
-  if (typeof reg == 'undefined') var reg
-   
-  polkamonList=document.getElementsByClassName('personalCollectionCardstyle__CardContainer-sc-61434w-0 gxEUbp');
-  reg=/(Chance ~)(.*)(%)/;
+if (typeof polkamonList == 'undefined') var polkamonList
+if (typeof reg == 'undefined') var reg
 
-  for(let polkamon of polkamonList){
-    let proba=1;
-    let spanList=polkamon.getElementsByTagName('span');
+polkamonList=document.getElementsByClassName('personalCollectionCardstyle__CardContainer-sc-61434w-0 gxEUbp');
+reg=/(Chance ~)(.*)(%)/;
 
-    for(let span of spanList){
-      let match = span.textContent.match(reg);
-      if(match){
-        proba*=(match[2]/100);
-      }
+for(let polkamon of polkamonList){
+  let proba=1;
+  let spanList=polkamon.getElementsByTagName('span');
+
+  for(let span of spanList){
+    let match = span.textContent.match(reg);
+    if(match){
+      proba*=(match[2]/100);
     }
-
-    polkamon.getElementsByClassName('personalCollectionCardstyle__DetailsText-sc-61434w-5 jYnmvc')[0].textContent=(Math.round(1/proba));
   }
-})();
+
+  polkamon.getElementsByClassName('personalCollectionCardstyle__DetailsText-sc-61434w-5 jYnmvc')[0].textContent=(Math.round(1/proba));
+}
