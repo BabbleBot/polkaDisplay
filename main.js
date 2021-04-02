396acf723adaf7329a1e9313ed26d8be7bc02f01
BabbleBot <35687156+BabbleBot@users.noreply.github.com>
2021-04-02 23:58:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-let polkamonList=document.getElementsByClassName('personalCollectionCardstyle__CardContainer-sc-61434w-0 gxEUbp');
-let reg=/(Chance ~)(.*)(%)/;
+(() => {
+  let polkamonList=document.getElementsByClassName('personalCollectionCardstyle__CardContainer-sc-61434w-0 gxEUbp');
+  let reg=/(Chance ~)(.*)(%)/;
 
-for(let polkamon of polkamonList){
-  let proba=1;
-  let spanList=polkamon.getElementsByTagName('span');
+  for(let polkamon of polkamonList){
+    let proba=1;
+    let spanList=polkamon.getElementsByTagName('span');
 
-  for(let span of spanList){
-    let match = span.textContent.match(reg);
-    if(match){
-      proba*=(match[2]/100);
+    for(let span of spanList){
+      let match = span.textContent.match(reg);
+      if(match){
+        proba*=(match[2]/100);
+      }
     }
-  }
 
-  polkamon.getElementsByClassName('personalCollectionCardstyle__DetailsText-sc-61434w-5 jYnmvc')[0].textContent=(Math.round(1/proba));
-}
+    polkamon.getElementsByClassName('personalCollectionCardstyle__DetailsText-sc-61434w-5 jYnmvc')[0].textContent=(Math.round(1/proba));
+  }
+})();
